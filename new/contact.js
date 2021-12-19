c1a8a28b8495158e182e323dafcfe66145293d28
sacheenShyama <nomadsachin2002@gmail.com>
2021-12-19 20:39:13 +0530

    working website

---
+++ new/contact.js
@@ -0,0 +1,42 @@
+
+    function hidedata(ele) {
+        let mum = document.getElementById("mum-add");
+        mum.style.display = mum.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata2(ele) {
+        let bang = document.getElementById("bang-add");
+        bang.style.display = bang.style.display == 'none' ? 'block' : 'none';
+
+    }
+    function hidedata3(ele) {
+        let hyd = document.getElementById("hyd");
+        hyd.style.display = hyd.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata4(ele) {
+        let jai = document.getElementById("jai");
+        jai.style.display = jai.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata5(ele) {
+        let guru = document.getElementById("guru");
+        guru.style.display = guru.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata6(ele) {
+        let mys = document.getElementById("mys");
+        mys.style.display = mys.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata7(ele) {
+        let pune = document.getElementById("pune");
+        pune.style.display = pune.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata8(ele) {
+        let uda = document.getElementById("uda");
+        uda.style.display = uda.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata9(ele) {
+        let ahm = document.getElementById("ahm");
+        ahm.style.display = ahm.style.display == 'none' ? 'block' : 'none';
+    }
+    function hidedata10(ele) {
+        let gur = document.getElementById("gur");
+        gur.style.display = gur.style.display == 'none' ? 'block' : 'none';
+    }
